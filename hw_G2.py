f26378004834c711a043540589e2e1cc6f158e0b
g-chistov <41797852+GeorgyChistov@users.noreply.github.com>
2019-04-26 00:34:23 +0300

    Create hw_G2.py

---
+++ hw_G2.py
@@ -0,0 +1,15 @@
+def prime_numb_gen(n):             #Функция генерирует n-e простое число
+    primes = [2]
+    add = 3
+    while len(primes) <= n-1:
+        flag = False           #True, когда число не простое(оно делится на какое-то число из списка)
+        for num in primes:
+            if add % num == 0:
+                flag = True
+        if flag:
+            add += 1
+            continue
+        else:
+            primes.append(add)
+            add += 1
+    print(primes[len(primes)-1])
