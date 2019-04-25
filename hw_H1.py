6b3c5268d2f843c07730c5c588a88c40032632ac
g-chistov <41797852+GeorgyChistov@users.noreply.github.com>
2019-04-26 02:08:49 +0300

    Update hw_H1.py

--- hw_H1.py
+++ hw_H1.py
@@ -1,29 +1,28 @@
 import time
 import random as rn
 
-force = [i for i in range(4,16,2)]
+force = [i for i in range(21)]                        #обозначаем интервал возможного урона
 class Player:
   def __init__(self, health):
     self.hp = health
-class Zombie(Player):
+class Zombie(Player):                                   #удар зомби, зомби лечитсяна некоторое количество поинтов при укусе жертвы,пусть будет 6
   def bite(self, target):
     jaw = rn.choice(force)
     target.hp -= jaw
-    self.hp += 1
+    self.hp += 6
     print('Oh no! Zombie bites him and damage ' + str(jaw) + '! Zombie get heal at 1')
 class Human(Player):
-  def hit(self,target):
-    force = [i for i in range(20)]
+  def hit(self,target):                                              #удар человека
     kick = rn.choice(force)
     print('Human attacks zombie and damage is ' + str(kick) + 'hp')
-    phrases = ["GREAT HIT!", "CRITICAL DAMAGE FROM HUMAN!!!", "KAWABANGA, HE ALMOST KILL IT!"]
+    phrases = ["GREAT HIT!", "CRITICAL DAMAGE FROM HUMAN!!!", "KAWABANGA, HE ALMOST KILL IT!"]      #в лучших традициях, всем приятно знать, что есть криты)
     if kick > 12:
       phrase = rn.choice(phrases)
       print(phrase)
     target.hp -= kick 
 rules = True
 while rules:  
-  zombie = Zombie(health = int(input('Enter zombie health: ')))
+  zombie = Zombie(health = int(input('Enter zombie health: ')))              #вводим хп для игроков.Зомби дхоляк, поэтому хп, большего, чем у человека, у него быть не может
   human = Human(health = int(input('Enter human health: ')))
   if zombie.hp < human.hp:
     break
@@ -31,7 +30,7 @@ while rules:
     print('Zombie cannot have more health, than human')
 fight = True
 
-while fight:
+while fight:                                                    #битва!
   time.sleep(0.5)
   human.hit(zombie)
   print('zombie = ' + str(zombie.hp))
