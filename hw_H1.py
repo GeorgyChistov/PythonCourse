ac8c1c8760c88d1c6af9aa3130fa7908e1661329
g-chistov <41797852+GeorgyChistov@users.noreply.github.com>
2019-04-26 02:02:56 +0300

    Update hw_H1.py

--- hw_H1.py
+++ hw_H1.py
@@ -1,10 +1,10 @@
-import time 
+import time
 import random as rn
 
 force = [i for i in range(4,16,2)]
 class Player:
-  def __init__(self, hp):
-    self.hp = hp
+  def __init__(self, health):
+    self.hp = health
 class Zombie(Player):
   def heal(self,points):
     self.hp += 5
@@ -12,17 +12,21 @@ class Zombie(Player):
     jaw = rn.choice(force)
     target.hp -= jaw
     self.hp += 1
+    print('Oh no! Zombie bites him and damage ' + str(jaw) + '! Zombie get heal at 1')
 class Human(Player):
   def hit(self,target):
-    force = [i for i in range(4,16,2)]
+    force = [i for i in range(20)]
     kick = rn.choice(force)
+    print('Human attacks zombie and damage is ' + str(kick) + 'hp')
+    phrases = ["GREAT HIT!", "CRITICAL DAMAGE FROM HUMAN!!!", "KAWABANGA, HE ALMOST KILL IT!"]
     if kick > 12:
-      print('Great hit!')
+      phrase = rn.choice(phrases)
+      print(phrase)
     target.hp -= kick 
-rules = False
-while not rules:  
-  zombie = Zombie(hp = int(input('Enter zombie health')))
-  human = Human(hp = int(input('Enter human health')))
+rules = True
+while rules:  
+  zombie = Zombie(health = int(input('Enter zombie health: ')))
+  human = Human(health = int(input('Enter human health: ')))
   if zombie.hp < human.hp:
     break
   else:
@@ -30,19 +34,27 @@ while not rules:
 fight = True
 
 while fight:
-  print('Human attacks!')
   time.sleep(0.5)
   human.hit(zombie)
   print('zombie = ' + str(zombie.hp))
   print('human = ' + str(human.hp))
   if human.hp <= 0 or zombie.hp <= 0:
-    break
+    if human.hp > 0:
+      print("Human is winner!")
+      break
+    else:
+      print("Zombie if winner")
+      break
   time.sleep(1)
-  print('Zombie bites him!')
   time.sleep(0.5)
   zombie.bite(human)
   print('zombie =' + str(zombie.hp))
   print('human =' + str(human.hp))
   if human.hp <= 0 or zombie.hp <= 0:
-    break
+        if human.hp > 0:
+      print("Human is winner!")
+      break
+    else:
+      print("Zombie if winner")
+      break
   time.sleep(1)
