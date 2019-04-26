16988f3500431020966a29c4800194767a2922a0
g-chistov <41797852+GeorgyChistov@users.noreply.github.com>
2019-04-26 20:25:03 +0300

    Update hw_E1.py

--- hw_E1.py
+++ hw_E1.py
@@ -1,8 +1,8 @@
 import numpy as np 
 import seaborn
 import matplotlib.pyplot as plt 
-lam_for_poisson = float(input())
-size_for_distribution = int(input())
+lam_for_poisson = float(input("Lam for poisson: "))
+size_for_distribution = int(input("Size_for_distribution: "))
 poisson_distribution = np.random.poisson(lam = lam_for_poisson, size = size_for_distribution) 
 seaborn.distplot (poisson_distribution,label = "poisson" ) 
 plt.legend () 
