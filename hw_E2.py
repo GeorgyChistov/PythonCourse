4d681aca54c2fcf579a0dd61682de04840b50f26
g-chistov <41797852+GeorgyChistov@users.noreply.github.com>
2019-04-26 20:23:53 +0300

    Update hw_E2.py

--- hw_E2.py
+++ hw_E2.py
@@ -1,9 +1,9 @@
-import seaborn as sea 
 import numpy as np 
-import matplotlib.pyplot as mathplt 
-
-pareto = np.random.pareto(100, 1000) 
-
-sea.distplot (pareto,label = "pareto") 
-mathplt.legend () 
-mathplt.show ()
+import seaborn
+import matplotlib.pyplot as plt 
+lam_for_pareto = float(input("Lam for pareto: "))
+size_for_distribution = int(input("Size for distribution: "))
+pareto_distribution = np.random.poisson(lam = lam_for_pareto, size = size_for_distribution) 
+seaborn.distplot (pareto_distribution,label = "pareto" ) 
+plt.legend () 
+plt.show ()
